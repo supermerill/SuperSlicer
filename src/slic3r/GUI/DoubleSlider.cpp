52461769671636caa75751a691bf0f0051cd5d46
Alice <alice@dn3s.me>
2023-10-01 23:38:17 -0500

    [gui/DoubleSlider] show pause print menu item when pause_print_gcode option is empty

--- src/slic3r/GUI/DoubleSlider.cpp
+++ src/slic3r/GUI/DoubleSlider.cpp
@@ -1987,7 +1987,6 @@ void Control::show_add_context_menu()
         append_add_color_change_menu_item(&menu);
     }
 
-    if (!gcode(PausePrint).empty())
     append_menu_item(&menu, wxID_ANY, _L("Add pause print") + " (" + gcode(PausePrint) + ")", "",
         [this](wxCommandEvent&) { add_code_as_tick(PausePrint); }, "pause_print", &menu);
 
